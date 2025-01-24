8cc9451c4c721b6f3badfc2e9341ebabc668b2db
Pius Opoku-Fofie <pius1919@gmail.com>
2025-01-24 17:32:46 +0000

    new login page

--- components/ui/MainLogo.tsx
+++ components/ui/MainLogo.tsx
@@ -1,32 +1,27 @@
-"use client"
+"use client";
 import { bebas, oswald } from '@/config/fonts';
 import { Avatar, Divider } from "@heroui/react";
-import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
 
-type Props = {};
+type MainLogoProps = {
+    textColor?: string; // Accepts a tailwind class for text color
+    logoSize?: string; // Accepts a tailwind class for logo size
+}
 
-const MainLogo = (props: Props) => {
+const MainLogo = ({ textColor = "text-purple-900 dark:text-purple-500", logoSize = "w-[5.4rem] h-[5.4rem]" }: MainLogoProps) => {
     return (
         <Link
             href={'/'}
-            className={`text-[1rem] leading-[1.2rem] font-normal flex h-5 items-center space-x-3 cursor-pointer`}
+            className="text-[1rem] leading-[1.2rem] font-normal flex h-5 items-center space-x-3 cursor-pointer"
         >
             <Avatar
-                className="w-[5.4rem] h-[5.4rem] text-large bg-purple-800 "
+                className={`${logoSize} text-large bg-purple-800 `}
                 src={'/images/costrad.png'}
             />
             <Divider orientation='vertical' className='bg-gray-300' />
-            <span
-                className={`${oswald.className} text-purple-900 dark:text-purple-500 uppercase `}
-            // style={{
-            //     background: 'linear-gradient(45deg, #ff0000, #0000ff)', // Replace with your desired gradient or background
-            //     WebkitBackgroundClip: 'text',
-            //     WebkitTextFillColor: 'transparent',
-            // }}
-            >
-                College of Sustainable  Transformation<br /> And Development - <span className='normal-case '>(COSTrAD)</span>
+            <span className={`${oswald.className} ${textColor} uppercase`}>
+                College of Sustainable  Transformation<br /> 
+                And Development - <span className='normal-case'>(COSTrAD)</span>
             </span>
         </Link>
     );
--- components/ui/auth/signin-button.tsx
+++ components/ui/auth/signin-button.tsx
@@ -1,6 +1,12 @@
 "use client"
+import { Button } from "@heroui/button"
+import { LucideArrowUpRight } from "lucide-react"
 import { signIn } from "next-auth/react"
- 
+
 export function SignInButton() {
-  return <button onClick={() => signIn()}>Login</button>
+  const isDark = false; // Define the isDark variable
+  return <Button onPress={() => signIn()} className='uppercase rounded-none bg-purple-700 text-white font-bold ' size={'sm'} variant="solid"
+    endContent={<LucideArrowUpRight className='bg-black  text-white' />}>
+    <span className='text-sm normal-case '>JOIN COSTrAD</span>
+  </Button>
 }
