6413a4203850ab3f035884c295c2f3839f918d0d
Pius Opoku-Fofie <pius1919@gmail.com>
2025-05-23 10:46:23 +0000

    now requiring tailwind code upgrade...2

--- components/ui/MainLogo.tsx
+++ components/ui/MainLogo.tsx
@@ -1,37 +1,43 @@
 "use client";
-import { bebas, oswald } from '@/config/fonts';
+import { bebas, oswald } from "@/config/fonts";
 import { Avatar, Divider } from "@heroui/react";
-import Link from 'next/link';
+import Link from "next/link";
 
 type MainLogoProps = {
-    textColor?: string; // Accepts a tailwind class for text color
-    logoSize?: string; // Accepts a tailwind class for logo size
-    hideText?: boolean; // Accepts a boolean to hide text
-}
+  textColor?: string; // Accepts a tailwind class for text color
+  logoSize?: string; // Accepts a tailwind class for logo size
+  hideText?: boolean; // Accepts a boolean to hide text
+};
 
-const MainLogo = ({ hideText = false, textColor = "text-purple-900 dark:text-purple-500", logoSize = "w-[3.2rem] h-[3.2rem]  sm:w-[5.4rem] sm:h-[5.4rem]" }: MainLogoProps) => {
-    return (
-        <div
-            className="flex h-auto  items-center space-x-1 sm:space-x-3 cursor-pointer divide-x-1 divide-dotted"
-        >
-            <Link href={'/'} >
-                <Avatar
-                    className={`${logoSize} text-large  `}
-                    src={'/images/costrad.png'}
-                />
-            </Link>
-            {!hideText && (
-                <div>
-                    {/* <Divider orientation='vertical' className='bg-gray-300' /> */}
-                    <div className={`${oswald.className} ${textColor} uppercase text-xs sm:text-medium pl-3
-                        sm:leading-[1.2rem] font-light `}>
-                        College of Sustainable  Transformation<br />
-                        And Development - <span className='normal-case font-medium'>(COSTrAD)</span>
-                    </div>
-                </div>
-            )}
+const MainLogo = ({
+  hideText = false,
+  textColor = "text-purple-900 dark:text-purple-500",
+  logoSize = "w-[3.2rem] h-[3.2rem]  sm:w-[5.4rem] sm:h-[5.4rem]",
+}: MainLogoProps) => {
+  return (
+    <div className="flex h-auto  items-center space-x-3 sm:space-x-3 cursor-pointer ">
+      <Link href={"/"}>
+        <Avatar
+          className={`${logoSize} text-large  `}
+          src={"/images/costrad.png"}
+        />
+      </Link>
+      {!hideText && (
+        <div>
+          {/* <Divider orientation='vertical' className='bg-gray-300' /> */}
+          <div
+            className={`${oswald.className} ${textColor} uppercase text-xs sm:text-medium pl-3 border-left 
+                    border-dotted border-l-indigo-500/50 dark:border-l-white/50 border-l-1  border-style-dotted                sm:leading-[1.2rem] font-light `}
+          >
+            College of Sustainable Transformation
+            <br />
+            And Development -{" "}
+            <span className="normal-case font-medium">(COSTrAD)</span>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MainLogo;
