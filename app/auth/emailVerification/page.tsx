39166d7fc204f1c379bf22fb57b40d67cfee1a07
Pius Opoku-Fofie <pius1919@gmail.com>
2025-05-26 05:52:30 +0000

    ...stable

--- app/auth/emailVerification/page.tsx
+++ app/auth/emailVerification/page.tsx
@@ -3,12 +3,12 @@
 import { Alert, AlertDescription } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { useSession } from "@/hooks/use-session";
 import { useRouter } from "next/navigation"; // Import useRouter
@@ -18,51 +18,55 @@ import Loading from "@/components/Loading";
 import { set } from "date-fns";
 
 export default function EmailVerification() {
-	const [signupEmail, setSignupEmail] = useState<string | null>(null);
-	const { session, isLoading } = useSession(); // Get the user's session
-	const router = useRouter(); // Initialize the router
+  const [signupEmail, setSignupEmail] = useState<string | null>(null);
+  const { session, isLoading } = useSession(); // Get the user's session
+  const router = useRouter(); // Initialize the router
 
+  // Redirect to home if user is already signed in
+  useEffect(() => {
+    const fetchData = async () => {
+      const emailFromStorage = await sessionStorage.getItem("signupEmail");
+      setSignupEmail(emailFromStorage);
+    };
+    fetchData();
+  }, []);
 
-	// Redirect to home if user is already signed in
-	useEffect(() => {
-		const fetchData = async () => {
-			const emailFromStorage = await sessionStorage.getItem("signupEmail");
-			setSignupEmail(emailFromStorage);
+  useEffect(() => {
+    if (session && session.userId) {
+      router.push("/");
+    }
+  }, [session, router]);
 
-		};
-		fetchData();
-	}, []);
+  if (isLoading) {
+    return <Loading />;
+  }
 
-	if (isLoading) {
-		return <Loading />;
-	}
+  // if (session && session.userId) {
+  // 	return router.push("/");
+  // }
 
-	if (session && session.userId) {
-		return router.push("/");
-	}
-
-
-	return (
-		<div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)]">
-			<Card className="w-[350px]">
-				<CardHeader>
-					<CardTitle>Verify Email to Continue ...</CardTitle>
-					<CardDescription>
-						An email has been sent to{" "}
-						<span className="font-bold text-green-500">{signupEmail}</span>.
-					</CardDescription>
-				</CardHeader>
-				<CardContent>
-					Visit your inbox to verify your email. If not in your inbox, kindly check your spam folder.
-				</CardContent>
-				<CardFooter>
-					<Button type="submit" className="w-full" asChild>
-						<Link href="/">Back to Website</Link>
-					</Button>
-				</CardFooter>
-			</Card>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-10rem)]">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Verify Email to Continue ...</CardTitle>
+          <CardDescription>
+            An email has been sent to{" "}
+            <span className="font-bold text-green-500">{signupEmail}</span>.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Visit your inbox to verify your email. If not in your inbox, kindly
+          check your spam folder.
+        </CardContent>
+        <CardFooter>
+          <Button type="submit" className="w-full" asChild>
+            <Link href="/">Back to Website</Link>
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
