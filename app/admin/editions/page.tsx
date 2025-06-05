ae016f997ccfff0fea0a75323f5ddcb740b71ebf
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-05 21:18:31 +0000

    ....updates..

--- app/admin/editions/page.tsx
+++ app/admin/editions/page.tsx
@@ -4,24 +4,35 @@ import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 import data from "./institute_mongo.json";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
   return (
     <div className="px-4 space-y-4">
-      {/* Map your data to the expected shape for DataTable */}
-      <DataTable
-        data={data.map((item, idx) => ({
-          header: item.name,
-          id: idx,
-          type: item.acronym || "",
-          status: item.active ? "Active" : "Inactive",
-          target: item.slug || "",
-          limit: item.seo || "",
-          reviewer: item.updated_at || "",
-        }))}
-      />
+      <Dialog>
+        <DialogTrigger>
+        Add Edition
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
