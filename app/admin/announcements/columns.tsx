e59fb31eaa3257f736f493fc55a826df5dcdd3fa
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-13 08:14:40 +0000

    ....finishing  edit function .....

--- app/admin/announcements/columns.tsx
+++ app/admin/announcements/columns.tsx
@@ -30,7 +30,7 @@ export const columns: ColumnDef<any>[] = [
         aria-label="Select row"
       />
     ),
-    enableSorting: false,
+    enableSorting: true,
     enableHiding: false,
   },
   {
