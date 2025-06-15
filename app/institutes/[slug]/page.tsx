cd001aff7fb3edb6bcf2fb8fee0fd234b4de9531
Pius Opoku-Fofie <pius1919@gmail.com>
2025-06-15 19:49:22 +0000

    ....finishing insititutes .....

--- app/institutes/[slug]/page.tsx
+++ app/institutes/[slug]/page.tsx
@@ -1,11 +1,12 @@
 // app/institutes/[slug]/page.tsx
 import { prisma } from '@/prisma/dbConnect';
 
-export default async function InstituteViewPage({ params }: { params: { slug: string } }) {
+export default async function InstituteViewPage(props: { params: Promise<{ slug: string }> }) {
+  const params = await props.params;
 
-    console.log(params.slug);
+  console.log(params.slug);
   const inst = await prisma.institute.findUnique({
-    where: { id: params.slug },
+    where: { slug: params.slug },
     select: { name: true },
   });
   if (!inst) return <div>Not found</div>;
